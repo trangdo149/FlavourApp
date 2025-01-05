43e15043b58675768a41743005766b3dee8928c4
Diviw <dinhthaovy03@gmail.com>
2025-01-05 20:20:52 +0700

    sua lai nhieu thu
    đâsdsadasda

--- app/src/main/java/com/example/flavourapp/IngredientModel.kt
+++ app/src/main/java/com/example/flavourapp/IngredientModel.kt
@@ -2,11 +2,12 @@ package com.example.flavourapp
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.io.Serializable
 
 @Parcelize
 data class IngredientModel(
     val ingrImage: Int,
     val ingrName: String,
     val ingrQuantity: String,
-    val ingrUnit: String
+    val ingrUnit: String,
 ) : Parcelable
--- app/src/main/java/com/example/flavourapp/RecipeDetailActivity.kt
+++ app/src/main/java/com/example/flavourapp/RecipeDetailActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.example.flavourapp.base.BaseActivity
 import com.example.flavourapp.databinding.ActivityRecipeDetailBinding
+import com.example.flavourapp.fragment.ProfileFragment
 
 class RecipeDetailActivity : BaseActivity() {
     private lateinit var binding: ActivityRecipeDetailBinding
@@ -22,9 +24,18 @@ class RecipeDetailActivity : BaseActivity() {
 
     private val stepsList = listOf(
         StepsModel("1", "Ngâm đậu gà 8 tiếng (qua đêm)"),
-        StepsModel("2", "Nấu với lửa to cho sôi, hớt bọt sạch kỹ rồi hạ nhỏ lửa đậy nắp liu riu 45 phút đến 1 tiếng là đậu chín mềm"),
-        StepsModel("3", "Lột 4 tép tỏi bỏ lõi, cho vào blender cùng đậu đã luộc và các nguyên liệu như trên"),
-        StepsModel("4", "Xay trong vài phút hoặc đến khi đạt độ mượt như ý muốn. Thêm nước hầm đậu nếu muốn sốt loãng hơn."),
+        StepsModel(
+            "2",
+            "Nấu với lửa to cho sôi, hớt bọt sạch kỹ rồi hạ nhỏ lửa đậy nắp liu riu 45 phút đến 1 tiếng là đậu chín mềm"
+        ),
+        StepsModel(
+            "3",
+            "Lột 4 tép tỏi bỏ lõi, cho vào blender cùng đậu đã luộc và các nguyên liệu như trên"
+        ),
+        StepsModel(
+            "4",
+            "Xay trong vài phút hoặc đến khi đạt độ mượt như ý muốn. Thêm nước hầm đậu nếu muốn sốt loãng hơn."
+        ),
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,10 +49,34 @@ class RecipeDetailActivity : BaseActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val ingredientAdapter = IngredientAdapter(ingredientList)
+
+        // Setting everything from begin
+        val recipe = intent.getParcelableExtra<ProfileFragment.Recipe>("recipe")
+
+        Glide.with(this)
+            .load(recipe?.imageRes)
+            .into(binding.imageView)
+
+        binding.textView.text = recipe?.title ?: "Không có tên"
+
+
+        val listIngredient = intent.getParcelableArrayListExtra<IngredientModel>("ingredient")
+        val listStep = intent.getParcelableArrayListExtra<StepsModel>("steps")
+
+        val ingredientAdapter = if (listIngredient != null) {
+            IngredientAdapter(listIngredient)
+        } else {
+            IngredientAdapter(ingredientList)
+        }
         binding.rvNguyenlieu.layoutManager = LinearLayoutManager(this)
         binding.rvNguyenlieu.adapter = ingredientAdapter
-        val stepsAdapter = StepsAdapter(stepsList)
+
+
+        val stepsAdapter = if (listStep != null) {
+            StepsAdapter(listStep)
+        } else {
+            StepsAdapter(stepsList)
+        }
         binding.rvCacbuoc.layoutManager = LinearLayoutManager(this)
         binding.rvCacbuoc.adapter = stepsAdapter
         binding.nestedScrollView.post {
